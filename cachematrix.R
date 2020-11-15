8d7892c28c63e72c306c13734306197bb7572755
mjddelacruz <michael.d.dela.cruz@gmail.com>
2020-11-16 10:15:02 +1100

    Update cachematrix.R
    Applied the necessary changes for the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## function that calculates the inverse of the matrix and store it in cache. If the set() function is called, this resets the cache.
+makeCacheMatrix <- function(x = matrix()){
+      inv_list <- NULL
+      set <- function(y){
+          x <<- y
+          inv_list <<- NULL
+      }
+      get <- function() {x}
+      setInverse <- function(inverse) {inv_list <<- inverse}
+      getInverse <- function() { 
+         if (!is.null(inv_list)) ## if NULL compute, this means matrix is set using the set() function
+         {
+            inver<-solve(x) #solve for inverse
+         }
+      }
+      list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Function that gets the inversed matrix and gets the cached data if using the same matrix value, else, recalcs it.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+      inv_list <- x$getInverse()
+      if(!is.null(inv_list)){  #check for cached data
+            message("getting cached data")
+            return(inv_list)
+      }
+      #calculate inverse
+      mat <- x$get()
+      inv_list <- solve(mat, ...)
+      x$setInverse(inv_list)
+      inv_list
 }
